36b47e61de9310f13af555f9789e90ecbc9376d8
Matheus Melo <matheus.almeida@ccc.ufcg.edu.br>
2019-10-06 16:27:38 -0300

    add delete_pĺugin support

--- refresher/components/worker_registry.py
+++ refresher/components/worker_registry.py
@@ -4,6 +4,15 @@ def get_plugins(*args, **kwargs):
     r = RefreshRequester('worker_registry')
     return r.block_request({"action": "get_plugins"})
 
+
+def delete_plugin(plugin_name, *args, **kwargs):
+    r = RefreshRequester('worker_registry')
+    return r.block_request({
+        "action": "get_plugins",
+        "plugin_name": plugin_name
+    })
+
 ACTIONS = {
-    "get_plugins": get_plugins
+    "get_plugins": get_plugins,
+    "delete_plugin": delete_plugin
 }
